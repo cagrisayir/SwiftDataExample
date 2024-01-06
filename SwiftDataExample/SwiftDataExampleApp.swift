58e5ffcde5d1b9908d6c640640390769d5d827af
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-06 13:57:10 +0300

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Omer Cagri Sayir on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
